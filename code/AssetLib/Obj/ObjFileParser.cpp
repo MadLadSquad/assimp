b24b2fdf90649e344120a49b7d10c76f1265b28a
Madman10K <stanv.contact176@gmail.com>
2022-07-20 18:49:26 +0300

    Update

--- code/AssetLib/Obj/ObjFileParser.cpp
+++ code/AssetLib/Obj/ObjFileParser.cpp
@@ -458,8 +458,9 @@ void ObjFileParser::getFace(aiPrimitiveType type) {
             iPos = 0;
         } else {
             //OBJ USES 1 Base ARRAYS!!!!
-            std::string number(&(*m_DataIt), m_DataItEnd - m_DataIt);
-            const int iVal(::atoi(number.c_str()));
+            const int iVal(::atoi(&(*m_DataIt)));
+            //std::string number(&(*m_DataIt), m_DataItEnd - m_DataIt);
+            //const int iVal(::atoi(number.c_str()));
 
             // increment iStep position based off of the sign and # of digits
             int tmp = iVal;
